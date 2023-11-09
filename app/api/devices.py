5a1601b9eb2c096cf5425bbb3b54005ce12141e8
owntheweb <67289492+owntheweb@users.noreply.github.com>
2023-11-09 06:08:23 -0700

    WIP: list devices, needs more work around filtering

--- app/api/devices.py
+++ app/api/devices.py
@@ -6,7 +6,7 @@
 #TODO sort
 
 from ..models import Device
-from ..model_enums import DeviceTypeEnum, UserRoleEnum
+from ..model_enums import DeviceTypeEnum, UserRoleEnum, DeviceStatusEnum
 from .. import db
 from .. import app
 from ..role_required import role_required
@@ -52,7 +52,7 @@ def create_device():
 @app.route("/api/devices/<deviceId>", methods=["PUT"])
 @jwt_required()
 def update_device(deviceId):
-    role_required([UserRoleEnum.ADMIN])
+    role_required([UserRoleEnum.ADMIN, UserRoleEnum.EDITOR])
     
     type = request.json.get("type", None).strip()
     name = request.json.get("name", None).strip()
@@ -101,3 +101,70 @@ def delete_device(deviceId):
         return jsonify(message='device deleted')
     except Exception:
         abort(500, 'an unknown error occurred')
+
+@app.route("/api/devices", methods=["GET"])
+@jwt_required()
+def read_devices():
+    # set order by
+    order_by = Device.name
+    match request.args.get('orderBy'):
+        case 'date':
+            order_by = Device.created_at
+        case 'type':
+            order_by = Device.type
+        case 'status':
+            order_by = Device.status
+
+    filters = []
+
+    # filter by device type
+    if request.args.get('type'):
+        valid_type = request.args.get('type') in [e.value for e in DeviceTypeEnum]
+        if (valid_type):
+            filters.append(Device.type == request.args.get('type'))
+
+    # filter by status
+    if request.args.get('status'):
+        valid_status = request.args.get('status') in [e.value for e in DeviceTypeEnum]
+        if (valid_status):
+            filters.append(Device.status == request.args.get('status'))
+
+    print(filters)
+
+    # TODO: filter by user assignment (with table join, once users have assignments to test)
+
+    # set order direction
+    if request.args.get('orderDir') == 'desc':
+        order_by = order_by.desc()
+
+    # page
+    page = 1
+    if (request.args.get('page')):
+        #print(request.args.get('page'))
+        page = int(request.args.get('page'))
+        print(page)
+
+    # TODO: consider a server default config, also for a max page count
+    per_page = 20
+    max_per_page = 100
+    if (request.args.get('perPage')):
+        per_page = int(request.args.get('perPage'))
+    
+    # TODO how do I dynamically chain filters with filters array here instead of hard-coded like it is below? 
+    results = Device.query \
+        .filter(Device.type == DeviceTypeEnum.MACHINE) \
+        .filter(Device.status == DeviceStatusEnum.FUNCTIONAL) \
+        .order_by(order_by) \
+        .paginate(page=page, per_page=per_page, max_per_page=max_per_page, error_out=False)
+    
+    devices = []
+    for device in results:
+        devices.append({
+            'id': device.id,
+            'name': device.name,
+            'type': device.type,
+            'status': device.status,
+            'createdAt': device.created_at.isoformat()
+        })
+    
+    return jsonify(devices=devices)
