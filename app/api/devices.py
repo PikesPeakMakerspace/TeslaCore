e619a0cc9a7870cf3209276ed5a1b76bfcb6170c
owntheweb <67289492+owntheweb@users.noreply.github.com>
2023-11-10 20:44:23 -0700

    add single device view with placeholders for external relationships

--- app/api/devices.py
+++ app/api/devices.py
@@ -12,11 +12,12 @@ from sqlalchemy import exc
 
 devices = Blueprint('devices', __name__)
 
+
 @app.route("/api/devices", methods=["POST"])
 @jwt_required()
 def create_device():
     role_required([UserRoleEnum.ADMIN])
-    
+
     type = request.json.get("type", None).strip()
     name = request.json.get("name", None).strip()
 
@@ -26,47 +27,53 @@ def create_device():
     valid_type = type in [e.value for e in DeviceTypeEnum]
     if (not valid_type):
         abort(422, 'invalid type')
-    
+
     try:
         # create
         device = Device(type=type, name=name)
         db.session.add(device)
         db.session.commit()
-        
+
         # get full data
         db.session.refresh(device)
 
-        return jsonify(id=device.id, name=device.name, type=device.type, created_at=device.created_at.isoformat())
+        return jsonify(
+            id=device.id,
+            name=device.name,
+            type=device.type,
+            created_at=device.created_at.isoformat()
+        )
     except exc.IntegrityError:
         abort(409, 'a device with that name already exists')
     except Exception:
         abort(500, 'an unknown error occurred')
 
+
 @app.route("/api/devices/<deviceId>", methods=["PUT"])
 @jwt_required()
 def update_device(deviceId):
     role_required([UserRoleEnum.ADMIN, UserRoleEnum.EDITOR])
-    
+
     type = request.json.get("type", None).strip()
     name = request.json.get("name", None).strip()
 
     if (not deviceId):
         abort(422, 'missing device id e.g. /api/devices/DEVICE-ID')
-    
+
     if (not type or not name):
         abort(422, 'missing type or name')
 
     valid_type = type in [e.value for e in DeviceTypeEnum]
     if (not valid_type):
         abort(422, 'invalid type')
-    
+
     try:
         # find
         device = Device.query.filter_by(id=deviceId).first()
 
         if not device:
             abort(404, 'unable to find a device with that id')
-        
+
         # update
         device.type = type
         device.name = name
@@ -74,19 +81,25 @@ def update_device(deviceId):
 
         # return the latest data in database
         db.session.refresh(device)
-        return jsonify(id=device.id, name=device.name, type=device.type, created_at=device.created_at.isoformat())
+        return jsonify(
+            id=device.id,
+            name=device.name,
+            type=device.type,
+            created_at=device.created_at.isoformat()
+        )
     except Exception:
         abort(500, 'an unknown error occurred')
 
-#TODO: Consider removing reference from other existing tables once those exist 
+
+# TODO: Consider removing reference from other existing tables once those exist
 @app.route("/api/devices/<deviceId>", methods=["DELETE"])
 @jwt_required()
 def delete_device(deviceId):
     role_required([UserRoleEnum.ADMIN])
-    
+
     if (not deviceId):
         abort(422, 'missing device id e.g. /api/devices/DEVICE-ID')
-    
+
     try:
         # delete
         Device.query.filter(Device.id == deviceId).delete()
@@ -94,6 +107,7 @@ def delete_device(deviceId):
         return jsonify(message='device deleted')
     except Exception:
         abort(500, 'an unknown error occurred')
+
 
 @app.route("/api/devices", methods=["GET"])
 @jwt_required()
@@ -112,19 +126,22 @@ def read_devices():
 
     # filter by device type
     if request.args.get('type'):
-        valid_type = request.args.get('type') in [e.value for e in DeviceTypeEnum]
+        valid_type = request.args.get('type') \
+            in [e.value for e in DeviceTypeEnum]
         if (valid_type):
-            #filters.append(Device.type == request.args.get('type'))
+            # filters.append(Device.type == request.args.get('type'))
             query = query.filter(Device.type == request.args.get('type'))
 
     # filter by status
     if request.args.get('status'):
-        valid_status = request.args.get('status') in [e.value for e in DeviceStatusEnum]
+        valid_status = request.args.get('status') \
+            in [e.value for e in DeviceStatusEnum]
         if (valid_status):
-            #filters.append(Device.status == request.args.get('status'))
+            # filters.append(Device.status == request.args.get('status'))
             query = query.filter(Device.status == request.args.get('status'))
 
-    # TODO: filter by user assignment (with table join, once users have assignments to test)
+    # TODO: filter by user assignment (with table join, once users have
+    # assignments to test)
 
     # set order direction
     if request.args.get('orderDir') == 'desc':
@@ -133,7 +150,7 @@ def read_devices():
     # page
     page = 1
     if (request.args.get('page')):
-        #print(request.args.get('page'))
+        # print(request.args.get('page'))
         page = int(request.args.get('page'))
         print(page)
 
@@ -145,8 +162,13 @@ def read_devices():
 
     results = query \
         .order_by(order_by) \
-        .paginate(page=page, per_page=per_page, max_per_page=max_per_page, error_out=False)
-    
+        .paginate(
+            page=page,
+            per_page=per_page,
+            max_per_page=max_per_page,
+            error_out=False
+        )
+
     devices = []
     for device in results:
         devices.append({
@@ -156,5 +178,53 @@ def read_devices():
             'status': device.status,
             'createdAt': device.created_at.isoformat()
         })
-    
+
     return jsonify(devices=devices)
+
+
+@app.route("/api/devices/<deviceId>", methods=["GET"])
+@jwt_required()
+def read_device_view(deviceId):
+    if (not deviceId):
+        abort(422, 'missing device id e.g. /api/devices/DEVICE-ID')
+
+    try:
+        # find
+        device = Device.query.filter_by(id=deviceId).first()
+
+        if not device:
+            abort(404, 'unable to find a device with that id')
+
+        view = {
+            'id': device.id,
+            'name': device.name,
+            'type': device.type,
+            'createdAt': device.created_at.isoformat(),
+            'status': device.status,
+            'upTime': 'soon',
+            'lastPinged': 'soon',
+            'accessNode': {
+                'id': 'soon',
+                'more': 'soon'
+            },
+            'userRecent': {
+                'id': 'soon',
+                'more': 'soon',
+            },
+            'usersWithAccess': [
+                {
+                    'id': 'soon',
+                    'more': 'soon',
+                }
+            ],
+            'accessHistory': [
+                {
+                    'id': 'soon',
+                    'more': 'soon'
+                }
+            ]
+        }
+
+        return jsonify(view=view)
+    except Exception:
+        abort(500, 'an unknown error occurred')
