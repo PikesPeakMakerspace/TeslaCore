79b04fdcbe451625c23e03ff21608c6ccc826ded
Christopher Stevens <67289492+owntheweb@users.noreply.github.com>
2023-11-11 08:04:59 -0700

    add single device view with placeholders for external relationships (#25)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,19 @@
-## v1.0.1
+## v1.1.0
+
 2023-11-09
-* created github actions
+
+-   archive devices not delete
+-   placeholder device log endpoint
+-   models formatting, cleanup
+
+## v1.0.1
+
+2023-11-09
+
+-   created github actions
 
 ## v.1.0.0
+
 2023-11-09
-* initial project setup
+
+-   initial project setup
--- app/model_enums.py
+++ app/model_enums.py
@@ -1,31 +1,39 @@
 import enum
 
-# TODO: Discuss: This assumes single role per user. Should we consider a list of roles per user? (and how would we go about that if relevant?)
+
+# TODO: Discuss: This assumes single role per user. Should we consider a list
+# of roles per user? (and how would we go about that if relevant?)
 class UserRoleEnum(str, enum.Enum):
     UNVERIFIED = 'unverified'
     USER = 'user'
     EDITOR = 'editor'
     ADMIN = 'admin'
-    # TODO: If a user can access everything a public display can, remove this role?:
+    # TODO: If a user can access everything a public display can, remove
+    # this role?:
     PUBLIC_DISPLAY = 'public display'
+
 
 class AccessCardStatusEnum(str, enum.Enum):
     FUNCTIONAL = 'functional'
     LOST = 'lost'
     STOLEN = 'stolen'
-    DECOMMISSIONED = 'decommissioned'
+    ARCHIVED = 'archived'
+
 
 class AccessNodeStatusEnum(str, enum.Enum):
     OFFLINE = 'offline'
     AVAILABLE = 'available'
     IN_USE = 'in use'
     ERROR = 'error'
-    DECOMMISSIONED = 'decommissioned'
+    ARCHIVED = 'archived'
+
 
 class DeviceTypeEnum(str, enum.Enum):
-    # TODO: "machine" may be too generalized, make a new enum value for each unique configuration a tesla node needs to work through
+    # TODO: "machine" may be too generalized, make a new enum value for each
+    # unique configuration a tesla node needs to work through
     MACHINE = 'machine'
     DOOR = 'door'
+
 
 class DeviceStatusEnum(str, enum.Enum):
     FUNCTIONAL = 'functional'
@@ -33,5 +41,5 @@ class DeviceStatusEnum(str, enum.Enum):
     OFFLINE = 'offline'
     LOST = 'lost'
     STOLEN = 'stolen'
-    DECOMMISSIONED = 'decommissioned'
     ON_LOAN = 'on loan'
+    ARCHIVED = 'archived'
