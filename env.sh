a5614ea717e3f8669521856b90eac26b430546d4
Pavan D <dayalpavan@gmail.com>
2023-11-16 20:43:03 -0700

    create makefile to setup env

--- env.sh
+++ env.sh
@@ -1,4 +1,10 @@
-export TESLA_JWT_SECRET_KEY="This is so secret people!"
 export FLASK_APP=app
 export FLASK_DEBUG=1
 source auth/bin/activate
+
+if [ -r secret.sh ]; then
+    source secret.sh
+    export GOT_API_SECRETS=1
+else
+    echo "error: please create secret.sh to define the api secrets"
+fi
